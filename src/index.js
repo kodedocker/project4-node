56fc75203c1a043966894294277cf57174feaa95
Santosh Mondal <santosh.ece06@gmail.com>
2021-03-11 13:11:33 +0530

    nodemon.added.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
 
-app.get("/", (req, res) => res.json({title:'Hello World'}));
+app.get("/", (req, res) => res.json({title:'Hello Worldd'}));
 
 app.listen(3000, ()=> console.log('Server Started!'));
