0dd239261531b3b6b07ba084974904b14b644baf
Santosh Mondal <santosh.ece06@gmail.com>
2021-03-11 15:40:44 +0530

    api updage.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
 
-app.get("/", (req, res) => res.json({title:'Hello Worldd'}));
+app.get("/", (req, res) => res.json({title:'Hello World Again'}));
 
 app.listen(3000, ()=> console.log('Server Started!'));
