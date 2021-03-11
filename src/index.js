e696ecc5708f03d39518ad084089ad3a4f2daa0f
Santosh Mondal <santosh.ece06@gmail.com>
2021-03-11 16:03:24 +0530

    main.branch.update.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
 
-app.get("/", (req, res) => res.json({title:'Hello World Again'}));
+app.get("/", (req, res) => res.json({title:'Hello World!!'}));
 
 app.listen(3000, ()=> console.log('Server Started!'));
